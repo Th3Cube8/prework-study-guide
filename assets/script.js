bc9994d48a1aeb7c730e14eaa4b67d2fc62626ca
Th3Cube8 <camryncobbs@gmail.com>
2024-09-22 01:21:50 -0700

    adding first JavaScript code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+function listTopics() {
 for(var x = 0; x < topics.length; x++) {
     console.log(topics[x])
 }
-/*
+}
+function selectTopic() {
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -14,4 +16,5 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
+}
+listTopics()
